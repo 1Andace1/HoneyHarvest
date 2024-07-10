33f8696d168aca960d6f9e12060ba8d65417ca64
Vladislav Startsev <teseu@tutanota.com>
2024-07-10 11:37:23 +0500

    new cards added

--- client/src/components/OneCard/OneCard.tsx
+++ client/src/components/OneCard/OneCard.tsx
@@ -9,10 +9,13 @@ import {
   Stack,
   Text,
   Image,
+  HStack,
+  VStack,
+  Box,
 } from "@chakra-ui/react";
 import { IProducts } from "../../types/stateTypes";
 
-export default function OneCard({ el }: {el: IProducts}): JSX.Element {
+export default function OneCard({ el }: { el: IProducts }): JSX.Element {
   const title = el.title;
   const price = el.price / 10;
   const discountRatio = el.discountRatio;
@@ -28,37 +31,41 @@ export default function OneCard({ el }: {el: IProducts}): JSX.Element {
 
   return (
     <div>
-      <Card maxW="sm">
-        <CardBody>
-          <Image
-            src={picture}
-            // src="/productsPhoto/105.jpeg"
-            alt="honey"
-            borderRadius="lg"
-          />
-          <Stack mt="6" spacing="3">
-            <Heading size="md">{title}</Heading>
-            <Text>{description}</Text>
-            <Text>Год урожая: {yearOfHarvest}</Text>
-            <Text>Сорт: {sort}</Text>
-            <Text>Местонахождение: {location}</Text>
-            <Text color="blue.600" fontSize="2xl">{price} руб. / 100 гр.</Text>
-            <Text color="blue.600" fontSize="2xl">Цена со скидкой: {priceConDiscountRatio} руб. / 100 гр.</Text>
-            <Text>Рейтинг: {starsRating}</Text>
-          </Stack>
-        </CardBody>
-        <Divider />
-        <CardFooter>
-          <ButtonGroup spacing="2">
-            <Button variant="solid" colorScheme="blue">
-              Купить
-            </Button>
-            <Button variant="ghost" colorScheme="blue">
-              Добавить в корзину
-            </Button>
-          </ButtonGroup>
-        </CardFooter>
-      </Card>
+        <Card maxW="sm">
+          <CardBody>
+            <Image
+              src={picture}
+              // src="/productsPhoto/105.jpeg"
+              alt="honey"
+              borderRadius="lg"
+            />
+            <Stack mt="6" spacing="3">
+              <Heading size="md">{title}</Heading>
+              <Text>{description}</Text>
+              <Text>Год урожая: {yearOfHarvest}</Text>
+              <Text>Сорт: {sort}</Text>
+              <Text>Местонахождение: {location}</Text>
+              <Text color="blue.600" fontSize="2xl">
+                {price} руб. / 100 гр.
+              </Text>
+              <Text color="blue.600" fontSize="2xl">
+                Цена со скидкой: {priceConDiscountRatio} руб. / 100 гр.
+              </Text>
+              <Text>Рейтинг: {starsRating}</Text>
+            </Stack>
+          </CardBody>
+          <Divider />
+          <CardFooter>
+            <ButtonGroup spacing="2">
+              <Button variant="solid" colorScheme="blue">
+                Купить
+              </Button>
+              <Button variant="ghost" colorScheme="blue">
+                Добавить в корзину
+              </Button>
+            </ButtonGroup>
+          </CardFooter>
+        </Card>
     </div>
   );
 }
--- client/src/pages/Catalog/CatalogPage.tsx
+++ client/src/pages/Catalog/CatalogPage.tsx
@@ -5,29 +5,35 @@ import { Heading } from "@chakra-ui/react";
 import { getProducts } from "../../redux/thunkActionsCatalog";
 import { ProductState } from "../../redux/types/states";
 import { IProducts } from "../../types/stateTypes";
+import { Stack, HStack, VStack, Box } from "@chakra-ui/react";
 // import { useNavigate } from 'react-router-dom';
 
 export default memo(function CatalogPage(): JSX.Element {
   const dispatch = useAppDispatch();
 
-  const { products } = useAppSelector((state: {productSlice: ProductState}) => state.productSlice);
+  const { products } = useAppSelector(
+    (state: { productSlice: ProductState }) => state.productSlice
+  );
 
   useEffect((): void => {
     dispatch(getProducts());
   }, []);
 
-console.log(products);
+  // console.log('products-------------++', products);
 
   return (
-  <>
-{ products.length ? products.map((el: IProducts) => 
-<OneCard el={el} key = {el.id} />
-)
-:
-<Heading as='h2' size='2xl'>
-Каталог пуст
-</Heading>
-}  
-</>
-  )
+    <>
+      {products.length ? (
+        products.map((el: IProducts) => (
+          <Stack direction="row" spacing="24px">
+              <OneCard el={el} key={el.id} />
+          </Stack>
+        ))
+      ) : (
+        <Heading as="h2" size="2xl">
+          Каталог пуст
+        </Heading>
+      )}
+    </>
+  );
 });
