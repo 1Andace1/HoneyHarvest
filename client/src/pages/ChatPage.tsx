2b4164a1853b3d84dfb6ceadcd9fd5107aac34f1
gazzzeles <gazelless@mail.ru>
2024-07-15 23:22:20 +0300

    mainPageV2

--- client/src/pages/ChatPage.tsx
+++ client/src/pages/ChatPage.tsx
@@ -63,6 +63,7 @@ const ChatPage: React.FC = () => {
               border: '1px solid black',
               width: '50px',
               height: '50px',
+              backgroundColor: "yellow"
             }}
           >
             {isChatVisible ? 'X' : '💬'}
