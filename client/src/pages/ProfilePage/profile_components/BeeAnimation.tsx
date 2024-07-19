2c90ad8fb80bb5b4ae96a4e42031312f317d46db
mariacez <mariavdovina89@gmail.com>
2024-07-19 13:29:00 +0300

    new style of orders

---
+++ client/src/pages/ProfilePage/profile_components/BeeAnimation.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './BeeAnimation.css';
+
+const BeeAnimation = () => {
+  return (
+    <div className="bee"></div>
+  );
+};
+
+export default BeeAnimation;
---
+++ client/src/pages/ProfilePage/profile_components/ProductChart.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+
+const ProductChart = ({ products }) => {
+  // Подготовка данных для графика
+  const labels = products.map(product => product.title);
+  const data = products.map(product => product.totalBasketPrice);
+
+  const chartData = {
+    labels,
+    datasets: [
+      {
+        label: 'Цена по товарам',
+        data,
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      tooltip: {
+        callbacks: {
+          label: function (context) {
+            return `${context.label}: ${context.formattedValue} руб.`;
+          },
+        },
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+      y: {
+        beginAtZero: true,
+        grid: {
+          display: false,
+        },
+      },
+    },
+  };
+
+  return <Bar data={chartData} options={options} />;
+};
+
+export default ProductChart;
