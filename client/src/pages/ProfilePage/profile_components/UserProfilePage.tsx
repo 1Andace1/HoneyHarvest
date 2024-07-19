f7e6e36c3d3635deef2f28ebe683428f1cfb4844
mariacez <mariavdovina89@gmail.com>
2024-07-19 10:12:56 +0300

    updated order cards

--- client/src/pages/ProfilePage/profile_components/UserProfilePage.tsx
+++ client/src/pages/ProfilePage/profile_components/UserProfilePage.tsx
@@ -1,3 +1,183 @@
+// import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
+// import { Box, useDisclosure } from '@chakra-ui/react';
+// import axiosInstance from '../../../axiosInstance';
+// import UserProfileCard from './UserProfileCard';
+// import EditProfileModal from './EditProfileModal';
+// // import LoyaltyInfo from './LoyaltyProgramComponent';
+// // import OrdersPage from './OrdersPageComponent';
+// import { useDispatch } from 'react-redux';
+// import { updateUser } from '../../../redux/slices/authSlice';
+
+// interface User {
+//   id: number;
+//   username: string;
+//   email: string;
+//   telephone: string;
+//   userCity: string;
+// }
+
+// interface FormData {
+//   username: string;
+//   email: string;
+//   password: string;
+//   telephone: string;
+//   userCity: string;
+//   profilePhoto: File | string;
+// }
+
+// interface UserProfilePageProps {
+//   user: User;
+//   userTotalSpent?: number;
+// }
+// const { VITE_API }: ImportMeta['env'] = import.meta.env;
+
+// const UserProfilePage: React.FC<UserProfilePageProps> = ({
+//   user,
+// }: UserProfilePageProps) => {
+
+//   const dispatch = useDispatch();
+//   const [formData, setFormData] = useState<FormData>({
+//     username: '',
+//     email: '',
+//     password: '',
+//     telephone: '',
+//     userCity: '',
+//     profilePhoto: '',
+//   });
+//   const { isOpen, onOpen, onClose } = useDisclosure();
+//   // const [error, setError] = useState<Error | null>(null);
+//   const [initialFormData, setInitialFormData] = useState<FormData>({
+//     username: '',
+//     email: '',
+//     password: '',
+//     telephone: '',
+//     userCity: '',
+//     profilePhoto: '',
+//   });
+
+//   useEffect(() => {
+//     const fetchUserData = async () => {
+//       try {
+//         const userData = await axiosInstance.get(
+//           `${VITE_API}/profile/user/${user.id}`
+//         );
+//         const initialData = {
+//           username: userData.data.username,
+//           email: userData.data.email,
+//           password: '',
+//           telephone: userData.data.telephone,
+//           userCity: userData.data.userCity,
+//           profilePhoto: '',
+//         };
+//         setFormData(initialData);
+//         setInitialFormData(initialData);
+//       } catch (error) {
+//         console.error('Ошибка при получении данных пользователя', error);
+//       }
+//     };
+
+//     // const fetchLoyaltyData = async () => {
+//     //   try {
+//     //     const loyaltyData = await axiosInstance.get(
+//     //       `${import.meta.env.VITE_API}/profile/loyalty`
+//     //     );
+//     //     setLoyalty(loyaltyData.data);
+//     //   } catch (error) {
+//     //     console.error('Ошибка при получении данных лояльности', error);
+//     //   }
+//     // };
+
+//     fetchUserData();
+//   }, [user.id]);
+
+//   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+//     const { name, value, files } = e.target;
+//     if (name === 'profilePhoto') {
+//          //  @ts-ignore
+//       setFormData({ ...formData, [name]: files[0] });
+//     } else {
+//       setFormData({ ...formData, [name]: value });
+//     }
+//   };
+
+//   // ~ обработчик отправки формы редактирования профиля
+//   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     try {
+//       // создание объекта updatedFields для хранения измененных полей
+//       const updatedFields: Partial<FormData> = {};
+
+//        // проверка измененные поля и добавляем их в updatedFields
+//       for (const key in formData) {
+//         if (
+//           formData[key as keyof FormData] !==
+//             initialFormData[key as keyof FormData] &&
+//           formData[key as keyof FormData] !== ''
+//         )
+//                       //  @ts-ignore
+//           updatedFields[key as keyof FormData] =
+//             formData[key as keyof FormData];
+
+//       }
+//      // Если нет изменений, выходим из функции
+//       if (Object.keys(updatedFields).length === 0) {
+//         console.log('No changes detected, form not submitted.');
+//         return;
+//       }
+
+//       const formDataObj = new FormData();
+//       for (const key in updatedFields) {
+//         formDataObj.append(
+//           key,
+//           updatedFields[key as keyof FormData] as string | Blob
+//         );
+//       }
+
+//       // проверка содержимого formDataObj
+//       for (const pair of formDataObj.entries()) {
+//         console.log(
+//           pair[0] + ': ' + (pair[1] instanceof Blob ? 'Blob' : pair[1])
+//         );
+//       }
+//       // отправка PUT запрос на сервер для обновления профиля
+//       const res = await axiosInstance.put(
+//         `${import.meta.env.VITE_API}/profile/users/${user.id}`,
+//         formDataObj,
+//         {
+//           headers: {
+//             Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+//             'Content-Type': 'multipart/form-data',
+//           },
+//         }
+//       );
+//       // Обновляем состояние пользователя на клиенте (тк Сервер вмдит изменения, а клиент нет)
+//       const updatedUser = res.data.user;
+//       dispatch(updateUser(updatedUser)); // Замените на ваш метод обновления пользователя в redux или в другом state management
+//       onClose();
+//     } catch (error: any) {
+//       // setError(error);
+//       console.error('Ошибка при обновлении данных пользователя', error);
+//     }
+//   };
+//   return (
+//     <Box p="0" m="0" border="none">
+//       {user && <UserProfileCard user={user} onEdit={onOpen} />}
+//       {/* {loyalty && <LoyaltyInfo userTotalSpent={userTotalSpent} />} */}
+//       {/* <OrdersPage user={user} /> */}
+//       <EditProfileModal
+//         isOpen={isOpen}
+//         onClose={onClose}
+//         //  @ts-ignore
+//         formData={formData}
+//         handleChange={handleChange}
+//         handleSubmit={handleSubmit}
+//       />
+//     </Box>
+//   );
+// };
+
+// export default UserProfilePage;
+
 import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
 import { Box, useDisclosure } from '@chakra-ui/react';
 import axiosInstance from '../../../axiosInstance';
@@ -29,11 +209,15 @@ interface UserProfilePageProps {
   user: User;
   userTotalSpent?: number;
 }
+
 const { VITE_API }: ImportMeta['env'] = import.meta.env;
 
 const UserProfilePage: React.FC<UserProfilePageProps> = ({
   user,
 }: UserProfilePageProps) => {
+  console.log(`user`, user);
+  console.log(`API Base URL: ${VITE_API}`);
+  console.log(` import.meta.env.VITE_API: ${import.meta.env.VITE_API}`);
 
   const dispatch = useDispatch();
   const [formData, setFormData] = useState<FormData>({
@@ -45,7 +229,6 @@ const UserProfilePage: React.FC<UserProfilePageProps> = ({
     profilePhoto: '',
   });
   const { isOpen, onOpen, onClose } = useDisclosure();
-  // const [error, setError] = useState<Error | null>(null);
   const [initialFormData, setInitialFormData] = useState<FormData>({
     username: '',
     email: '',
@@ -59,7 +242,7 @@ const UserProfilePage: React.FC<UserProfilePageProps> = ({
     const fetchUserData = async () => {
       try {
         const userData = await axiosInstance.get(
-          `${VITE_API}/profile/user/${user.id}`
+          `  http://localhost:3000/${import.meta.env.VITE_API}/profile/users/${user.id}`
         );
         const initialData = {
           username: userData.data.username,
@@ -76,50 +259,35 @@ const UserProfilePage: React.FC<UserProfilePageProps> = ({
       }
     };
 
-    // const fetchLoyaltyData = async () => {
-    //   try {
-    //     const loyaltyData = await axiosInstance.get(
-    //       `${import.meta.env.VITE_API}/profile/loyalty`
-    //     );
-    //     setLoyalty(loyaltyData.data);
-    //   } catch (error) {
-    //     console.error('Ошибка при получении данных лояльности', error);
-    //   }
-    // };
-
     fetchUserData();
   }, [user.id]);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value, files } = e.target;
     if (name === 'profilePhoto') {
-         //  @ts-ignore
-      setFormData({ ...formData, [name]: files[0] });
+      // @ts-ignore
+      setFormData({ ...formData, [name]: files ? files[0] : '' });
     } else {
       setFormData({ ...formData, [name]: value });
     }
   };
 
-  // ~ обработчик отправки формы редактирования профиля
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      // создание объекта updatedFields для хранения измененных полей
       const updatedFields: Partial<FormData> = {};
 
-       // проверка измененные поля и добавляем их в updatedFields
       for (const key in formData) {
         if (
           formData[key as keyof FormData] !==
             initialFormData[key as keyof FormData] &&
           formData[key as keyof FormData] !== ''
-        ) 
-                      //  @ts-ignore
+        ) {
           updatedFields[key as keyof FormData] =
             formData[key as keyof FormData];
-        
+        }
       }
-     // Если нет изменений, выходим из функции
+
       if (Object.keys(updatedFields).length === 0) {
         console.log('No changes detected, form not submitted.');
         return;
@@ -139,7 +307,7 @@ const UserProfilePage: React.FC<UserProfilePageProps> = ({
           pair[0] + ': ' + (pair[1] instanceof Blob ? 'Blob' : pair[1])
         );
       }
-      // отправка PUT запрос на сервер для обновления профиля
+
       const res = await axiosInstance.put(
         `${import.meta.env.VITE_API}/profile/users/${user.id}`,
         formDataObj,
@@ -150,15 +318,15 @@ const UserProfilePage: React.FC<UserProfilePageProps> = ({
           },
         }
       );
-      // Обновляем состояние пользователя на клиенте (тк Сервер вмдит изменения, а клиент нет)
+
       const updatedUser = res.data.user;
-      dispatch(updateUser(updatedUser)); // Замените на ваш метод обновления пользователя в redux или в другом state management
+      dispatch(updateUser(updatedUser));
       onClose();
-    } catch (error: any) {
-      // setError(error);
+    } catch (error) {
       console.error('Ошибка при обновлении данных пользователя', error);
     }
   };
+
   return (
     <Box p="0" m="0" border="none">
       {user && <UserProfileCard user={user} onEdit={onOpen} />}
@@ -167,7 +335,6 @@ const UserProfilePage: React.FC<UserProfilePageProps> = ({
       <EditProfileModal
         isOpen={isOpen}
         onClose={onClose}
-        //  @ts-ignore
         formData={formData}
         handleChange={handleChange}
         handleSubmit={handleSubmit}
